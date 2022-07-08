508918044629531438fdc188e2e958db17336d99
Steve Shickles <sshickles@zynga.com>
2022-07-08 13:19:20 -0500

    Day 1 project - band name generator

---
+++ Day_1/BandNameGenerator.py
@@ -0,0 +1,8 @@
+# Welcome message to user.
+print("Welcome to Steve's awesome Band Name generator!")
+# Get city from user.
+city = input("What city did you grow up in?\n")
+# Get pet name from user.
+pet = input("What is your pet's name?\n")
+# Combine city and name for band name.
+print(("Your new band's name is: ") + (city) + " " + (pet))
